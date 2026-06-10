43174403491aaaae5661cd96ae034bb5c7a1bef0
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-06-10 17:59:20 -0300

    feat(app): add language option

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,5 @@
 import './index.css'
+import SwitchLanguageButton from './components/ui/SwitchLanguageButton';
 
 const partOfTheDay = 'Afternoon';
 const userName = 'Joao';
@@ -9,27 +10,28 @@ function App() {
 
   return (
     <>
-      <header className='w-full h-fit flex bg-surface border border-px border-line rounded-xl px-4 py-4 items-center'>
-        <div className='flex-1 h-full flex flex-col gap-2 mr-4'>
-          <h1 className='text-ink font-semibold mb- text-2xl w-fit'>Good {partOfTheDay ?? '!'}, {userName ?? 'User'}</h1>
-          <p className='text-muted text-sm w-fit'>{date?? '-' }</p>
-          <div className='w-fit flex gap-2 bg-amber/10 border border-px border-amber/22 rounded-full px-2 py-1'>
-            <span className='text-amber text-sm'>Streak:</span>
-            <p className='text-amber text-sm'>{streakDays ?? '-'}</p>
-            <span className='text-amber text-sm'>Days</span>
+      <div className='w-full h-full flex flex-col gap-4'>
+        <header className='w-full h-fit flex bg-surface border border-px border-line rounded-xl px-4 py-4 items-center'>
+          <div className='flex-1 h-full flex flex-col gap-2 mr-4'>
+            <h1 className='text-ink font-semibold mb- text-xl w-fit'>Good {partOfTheDay ?? '!'}, {userName ?? 'User'}</h1>
+            <p className='text-muted text-sm w-fit'>{date?? '-' }</p>
+            <div className='w-fit flex gap-2 bg-amber/10 border border-px border-amber/22 rounded-full px-2 py-1'>
+              <p className='text-amber text-sm'>Streak: <span className='font-semibold'>{streakDays ?? '-'} </span>Days</p>
+            </div>
           </div>
+
+          {/* Core Habits Counting */}
+          <div className='size-22 rounded-full'></div>
+        </header>
+
+        {/* Language Selector */}
+        <div className='w-full flex gap-2 items-center justify-end '>
+          <SwitchLanguageButton language='EN' isSelected />
+          <SwitchLanguageButton language='PT' />
         </div>
-
-        {/* Core Habits Counting */}
-        <div className='size-22 rounded-full'></div>
-      </header>
-
-      {/* Pages selection */}
-      <nav></nav>
-
-
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
