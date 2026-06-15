7e8354a21ea7fd6c0bae46e46cd73acdd2558565
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-06-15 16:27:32 -0300

    feat: update Task to support TaskGroup relationship

--- back-end/src/main/java/joaodearaujo/daily_system/dto/request/TaskRequest.java
+++ back-end/src/main/java/joaodearaujo/daily_system/dto/request/TaskRequest.java
@@ -3,6 +3,7 @@ package joaodearaujo.daily_system.dto.request;
 import joaodearaujo.daily_system.domain.enums.TaskTags;
 
 public record TaskRequest(
+    String groupId,
     TaskTags tag,
     String name,
     String description,
