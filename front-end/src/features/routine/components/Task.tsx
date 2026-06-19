b8d24dd509286c7c005491c27da945a2664aa5c4
joaodearaujo <joao.pedrocdearaujo@gmail.com>
2026-06-18 23:59:43 -0300

    refactor(front-end): update Task and TaskGroup to use API data

--- front-end/src/features/routine/components/Task.tsx
+++ front-end/src/features/routine/components/Task.tsx
@@ -2,43 +2,38 @@ import { useState } from "react";
 import { cn } from "../../../utils/cn";
 import { ExpandButton } from "./ExpandButton";
 import { CheckButton } from "./CheckButton";
+import type { Task as TaskType} from "../types/routine.domain.type";
 import { CATEGORY_COLORS, type CategoryKey } from "../../../constants/categoryColors";
+import { useEditMode } from "../../../context/EditModeContext";
+import { DeleteButton } from "../../../components/DeleteButton";
+import { EditItemButton } from "../../../components/EditItemButton";
+import { Star } from "../../../components/Star";
+import { Dot } from "../../../components/Dot";
 
-type taskCategories = CategoryKey;
-
-export interface TaskProps {
-    id: string;
-    category: taskCategories;
-    title: string;
-    description: string;
-    isMandatory: boolean;
+interface Props {
+    task: TaskType;
 }
 
-export function Task({
-    category,
-    title,
-    description,
-    isMandatory = false
-}: TaskProps) {
+export function Task({ task }: Props) {
 
-    const categoryColor = CATEGORY_COLORS[category as CategoryKey] || CATEGORY_COLORS.STUDY;
-
+    const categoryColor = CATEGORY_COLORS[task?.category as CategoryKey] || CATEGORY_COLORS.STUDY;
     const [ isChecked, setIsChecked ] = useState(false)
     const [ isExpanded, setIsExpanded ] = useState(false)
+    const { isEditMode } = useEditMode();
 
     const controlCheck = (e: React.MouseEvent) => {
         e.stopPropagation();
         setIsChecked(prev => !prev);
-    }
+    };
 
     const controlExpand = (e: React.MouseEvent) => {
         e.stopPropagation();
         setIsExpanded(prev => !prev);
-    }
+    };
 
     const handlelExpand = () => {
         setIsExpanded(prev => !prev);
-    }
+    };
 
     return (
         <div className="w-full h-fit flex flex-col items-start bg-surface rounded-xl border border-line overflow-hidden">
@@ -46,35 +41,36 @@ export function Task({
                 onClick={handlelExpand}
                 className="w-full h-12 flex items-center jutify-between p-4 gap-2"
              >
-                <div
-                    style={{backgroundColor: categoryColor}} 
-                    className={cn("size-2 bg-green-400 rounded-full",
-                    )}
-                />
+                <Dot color={categoryColor}/>
                 <div className="flex-1 text-left flex items-center gap-2">
                     <span className={cn(
-                        "transition-colors text-[15px] font-medium font-secondary duration-200 ease-in-out",
+                        "transition-colors text-[15px] font-medium font-primary duration-200 ease-in-out",
                          isChecked ? 'line-through text-muted' : 'text-ink')}
                     >
-                        {title ?? 'Title not found'}
+                        {task?.title ?? 'Title not found'}
                     </span>
                     
-                    {isMandatory && <span className="text-amber text-xs">★</span>}
+                    {task?.isMandatory && <Star/>}
                 </div>
                 
-                <ExpandButton
-                    className="mr-2"
-                    onClick={controlExpand}
-                    isExpanded={isExpanded}
-                />
-                <CheckButton
-                    onClick={controlCheck}
-                    isChecked={isChecked}/>
+                {
+                    isEditMode 
+                    
+                    ? <>
+                        <EditItemButton />
+                        <DeleteButton />
+                      </> 
+                        
+                    : <>
+                        <ExpandButton className="mr-2" onClick={controlExpand} isExpanded={isExpanded}/>
+                        <CheckButton onClick={controlCheck} isChecked={isChecked}/> 
+                      </> 
+                }
             </div>
             {
-                isExpanded &&
+                isExpanded && !isEditMode &&
                     <div className="text-[13px] w-full text-left text-ink leading-5 pr-3.5 pb-3.5 pl-8.5">
-                        <p>{description ?? 'Description not found'}</p>
+                        <p>{task?.description ?? 'Description not found'}</p>
                     </div>
             }
         </div>
--- front-end/src/features/routine/components/TaskGroup.tsx
+++ front-end/src/features/routine/components/TaskGroup.tsx
@@ -1,22 +1,27 @@
 import { Task } from "./Task";
-import { tasks } from "../../../constants/tasks";
-import type { TaskGroup } from "../types/routine.domain";
+import type { TaskGroup } from "../types/routine.domain.type";
+import { AddButton } from "./AddButton";
+import { useEditMode } from "../../../context/EditModeContext";
 
-export function TaskGroup({ 
-    title, 
-    description,
-}: TaskGroup) {
+interface Props {
+    group: TaskGroup;
+}
+
+export function TaskGroup({ group }: Props) {
+
+        const { isEditMode } = useEditMode();
+
     return (
-        <div className="text-left text-muted text-xs flex flex-col gap-2 text-3">
+        <div className="text-left text-muted text-xs flex flex-col gap-2 text-3 border p-2.5 rounded-xl border-line">
             <div>
-                <h2 className="uppercase pb-1 font-secondary font-medium tracking-wide">{title}</h2>
-                <p className="leading-5">{description}</p>
+                <h2 className="uppercase pb-1 font-secondary font-semibold tracking-wider text-ink">{group?.title}</h2>
+                <p className="leading-5">{group?.description}</p>
             </div>
-            {tasks.map(task => <Task 
-                                    key={task.id} 
-                                    {...task}
-                                />
-                            )}
+            {group.tasks.map(task => (
+                <Task key={task.id} task={task} />
+            ))}
+            {isEditMode && <AddButton title="Add Task"/>}
+           
         </div>
     )
 }
